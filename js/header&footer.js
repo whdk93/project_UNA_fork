4e2f4e1134ebee519b867d83bddfd7c5c50dc865
inHoo <59555684+inhoo-han@users.noreply.github.com>
2023-03-13 23:40:57 +0900

    index_css_js

---
+++ js/header&footer.js
@@ -0,0 +1,80 @@
+$(function() {
+    $('.tMenu>.menu').hover(function() {
+        $(this).find('.subMenu').stop().slideDown(1000);
+        $(this).find('a').css(color = '#fff');
+    }, function() {
+        $(this).find('.subMenu').stop().slideUp(1000);
+    });
+    $('.home').click(function() {
+        window.location = 'index.html'
+    });
+
+    let loginSW = false;
+    $('#login_area .srch_btn').click(function() {
+        // $('#header_search').slideToggle("fast");
+        loginSW = !loginSW;
+        if(loginSW) {
+            $('#header_search').animate({
+                top: '100px'
+            });
+        }else {
+            $('#header_search').animate({
+                top: '0px'
+            });
+        }
+    });
+
+    /* * * * * hover시 color변경 영역 * * * * */
+    /* 서브메뉴 hover */
+    $('.smenu>a').hover(function() {
+        $(this).css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).css({
+            color: '#ccc'
+        });
+    });
+    /* 탑메뉴 hover */
+    $('.menu').hover(function() {
+        $(this).children('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).children('a').css({
+            color: '#ccc'
+        });
+    });
+    /* 로그인 영역 hover */
+    $('#login_area li').hover(function() {
+        $(this).find('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).find('a').css({
+            color: '#ccc'
+        });
+    });
+    /* footer link영역 hover */
+    $('#footer_link li').hover(function() {
+        $(this).find('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).find('a').css({
+            color: '#ccc'
+        });
+    });
+    /* 로그인 영역 돋보기 hover */
+    $('#login_area .srch_btn').hover(function() {
+        $('.srch_img').css({
+            filter: 'invert(100%)'
+        });
+    }, function() {
+        $('.srch_img').css({
+            filter: 'invert(80%)'
+        });
+    });
+
+
+});
